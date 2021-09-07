5d097a3eb7ae8f9d0023a9734ddb418b74449a2a
hiroshi sano <23502+hrsano645@users.noreply.github.com>
2021-09-07 20:09:54 +0900

    broadcast作成もできるようになった

--- exam/exam_youtube_create_broadcast.py
+++ exam/exam_youtube_create_broadcast.py
@@ -9,12 +9,15 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import requests
+
+import datetime
 from pathlib import Path
 import base64
 
 # TODO:2021-09-05 base64でエンコードしたclient_secret.jsonをロードする
 dotenv.load_dotenv()
+YT_STREAM_ID = os.environ["YT_STREAM_ID"]
+
 # client_secret_file = os.environ[""]
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
@@ -47,23 +50,86 @@ def get_authenticated_service():
 
 def main(credentials):
     youtube = credentials
-    # ref:https://github.com/youtube/api-samples/blob/master/python/list_streams.py
+    # ref:https://github.com/youtube/api-samples/blob/master/python/create_broadcast.py
+    options_broadcast_title = "pyconshizu api test " + datetime.datetime.now().strftime("%Y%m%d%H%M")
+    options_start_time = "2021-09-08T00:00:00.0Z"
+    options_broadcast_min = 40
+    options_privacy_status = "unlisted"
     try:
-        
-        list_streams_request = youtube.liveStreams().list(
-            part='id,snippet',
-            mine=True,
-            maxResults=50
-        )
-        # ページネーション的な処理っぽい。一度に得られる結果は限られるので
-        while list_streams_request:
-            list_streams_response = list_streams_request.execute()
+        # とりあえず決め打ちでの例
+        insert_broadcast_response = youtube.liveBroadcasts().insert(
+            part="snippet,status,contentDetails",
+            body={
+                "contentDetails": {
+                    "enableClosedCaptions": True,
+                    "enableContentEncryption": True,
+                    "enableDvr": True,
+                    "enableEmbed": False,
+                    "recordFromStart": True,
+                    "enableAutoStart": True,
+                    "enableAutoStop": True,
+                    "startWithSlate": True,
+                },
+                "snippet": {
+                    "title": options_broadcast_title,
+                    "scheduledStartTime": options_start_time,
+                },
+                "status": {
+                    "selfDeclaredMadeForKids": False,
+                    "privacyStatus": options_privacy_status
+                }
+            }
+        ).execute()
 
-            for stream in list_streams_response.get('items', []):
-                print('%s (%s)' % (stream['snippet']['title'], stream['id']))
+        b_snippet = insert_broadcast_response["snippet"]
 
-            list_streams_request = youtube.liveStreams().list_next(
-                list_streams_request, list_streams_response)
+        print("Broadcast '%s' with title '%s' was published at '%s'." % (
+            insert_broadcast_response["id"], b_snippet["title"], b_snippet["publishedAt"]))
+
+        # broadcastの設定をする
+
+        # update_broadcast_request = youtube.liveBroadcasts().update(
+        #     part="snippet,status,contentDetails",
+        #     body={
+        #         "id": insert_broadcast_response["id"],
+        #         # "contentDetails": {
+        #         #     "enableClosedCaptions": True,
+        #         #     "enableContentEncryption": True,
+        #         #     "enableDvr": True,
+        #         #     "enableEmbed": False,
+        #         #     "recordFromStart": True,
+        #         #     "enableAutoStart": True,
+        #         #     "enableAutoStop": True,
+        #         #     "startWithSlate": True,
+        #         # },
+        #         "snippet": {
+        #             "title": options_broadcast_title,
+        #             "scheduledStartTime": options_start_time,
+        #             # "scheduledEndTime": options_end_time
+        #         },
+        #         "status": {
+        #             "privacyStatus": options_privacy_status
+        #         }
+        #     }
+        # ).execute()
+
+        # cb_snippet = update_broadcast_request["snippet"]
+
+        # print("Broadcast '%s' with title '%s' was updated ." % (
+        #     update_broadcast_request["id"], cb_snippet["title"])
+        # )
+
+        # streamをbindする
+        # ストリーミングの情報はすでにあるので、それを使う
+        bind_broadcast_response = youtube.liveBroadcasts().bind(
+            part="id,contentDetails",
+            id=insert_broadcast_response["id"],
+            streamId=YT_STREAM_ID
+        ).execute()
+
+        print("Broadcast '%s' was bound to stream '%s'." % (
+            bind_broadcast_response["id"],
+            bind_broadcast_response["contentDetails"]["boundStreamId"]))
 
     except HttpError:
         import traceback
@@ -71,3 +137,12 @@ def main(credentials):
 
 if __name__ =="__main__":
     main(get_authenticated_service())
+
+
+# 気になること
+
+"""
+- youtube livestreming apiの操作で（というかGoogleのAPIライブラリの使い方として
+  partのオプションを指定するとき、送るbodyに入れたいデータの名称としては必ず入れないといけないのを注意すること
+
+"""
